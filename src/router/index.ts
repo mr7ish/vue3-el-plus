8584064480654f0fad830d7e586f28f8b7aafe67
cgm <377342130@qq.com>
2024-01-10 17:31:25 +0800

    feat: test router guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,18 @@ import routes from "./routes";
 const router = createRouter({
     history: createWebHashHistory(),
     routes
-})
+});
+
+router.onError((err) => {
+    console.log('router err =>', err);
+});
+
+router.beforeEach((to, from) => {
+    console.log('to =>', to);
+    console.log('from =>', from);
+
+    if(!to.path.includes('red')) return;
+    else return '/pagea';
+});
 
 export default router;
