11adbd61b7d3a5489a9ad6be6b728ab42c36811e
cgm <377342130@qq.com>
2024-01-22 10:45:56 +0800

    feat: use transition

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="fade">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -9,5 +13,13 @@
 </script>
 
 <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
 
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
